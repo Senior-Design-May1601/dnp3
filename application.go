31abd205ba90e935253d823d25eb5dda35e0982d
Crazzytrain36 <Jonathan.Osborne36@gmail.com>
2016-03-21 17:14:30 -0500

    Started adding g120v3 and response headers. debugged projectmain issues

--- application.go
+++ application.go
@@ -17,3 +17,35 @@ func AppRead(data []byte) AppLayer_t{
 	return AppLayer_t{func_code}
 
 }
+
+func ApplicationResponse()[]byte{
+
+
+}
+
+func makeG120()[]byte {
+ 	var object []byte
+	object[0] = 120 //group number
+	object[1] = 1 // variation
+	object[2] = 0 // no prefix
+	
+	//begin arbitrary sequence num
+	object[3] = 34
+	object[4] = 3
+	object[5] = 100
+	object[6] = 64
+	//end sequenc num
+	
+	object[7] = 0 //usr number unknown because outstation
+	object[8] = 1 // sha-1  4 bytes
+	object[9] = 1 // reason for challenge = Critical adsu
+
+	//begin challenge data 4 bytes sha-1 could make these random
+	object[10] = 0xFE
+	object[11] = 0x00
+	object[12] = 0x33
+	object[13] = 0xAB
+	//end challenge	
+	return object
+}
+
--- main.go
+++ main.go
@@ -2,50 +2,87 @@ package main
 import(
 	"net"
 	"log"
-	"fmt"
 	"io"
 	"bytes"
+	"os"	
+	"flag"
+	"strconv"
 	
-
 	"github.com/Senior-Design-May1601/Splunk/alert"
 	"github.com/Senior-Design-May1601/projectmain/logger"
+	"github.com/BurntSushi/toml"
 )
 
-type Headers struct{
-	DataLink DataLayer_t
-	AppLayer AppLayer_t
+type Config struct {
+	Address string
+	Port int
 }
 
-func handler(c net.Conn){
-	defer c.Close()
+
+func handler(c net.Conn,mylogger *log.Logger){
 	var buf bytes.Buffer
 	io.Copy(&buf, c)
-	fmt.Println(buf.String())
+	defer c.Close()
 
-// END GET APPLICATION DATA
+//Connection Data
+	remoteAddr := c.RemoteAddr()
+	localAddr := c.LocalAddr()
 
-	transport_data,d_struct :=DataLinkRead(buf.Bytes()) // Strip DataLink Header
-	app_data:=TransportRead(transport_data)
-	app_struct := AppRead(app_data)
-	data := Headers{d_struct,app_struct}		
-	mylogger.Println(makeAlert(data))	
+//If incorrect preamble send unknown alert
+/*	if buf[0] != 0x05 && buf[1] != 0x64{
+		makeUnknownAlert(remoteAddr,localAddr)
+	}*/
+
+
+ // Just a logger
+	fileLog := logger.NewLogger("",0)
+
+	f, _ := os.OpenFile("dnp-log-file",  os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)	
+	defer f.Close()
+	fileLog.SetOutput(f)
+	
+
+	resp,dl :=DataLinkRead(buf.Bytes()) // Strip DataLink header
+	resp = TransportRead(resp) //Strip Transport header
+	ap:=AppRead(resp) //Strip Application header
+
+	str := makeAlert(dl,ap,remoteAddr,localAddr)
+	fileLog.Println(str)
+	mylogger.Println(str)
+	
 }
-func makeAlert(h Headers) string{
+func makeAlert(dl DataLayer_t, app AppLayer_t, remoteAddr net.Addr,localAddr net.Addr) string{
 	meta := make(map[string]string)
-	meta["source"] = h.DataLink.source
-	meta["origin"] = h.DataLink.origin
-	meta["dlayer_code"] = h.DataLink.f_code
-	meta["alayer_code"] = h.AppLayer.function_code
-		
+	meta["machine_source"] = dl.source
+	meta["origin"] = dl.origin
+	meta["ln_function_code"] = dl.f_code
+	meta["ap_function_code"] = app.function_code
+	meta["remote_address"] = remoteAddr.String()
+	meta["local_address"] = localAddr.String()
+	resp :=  alert.NewSplunkAlertMessage(meta)
+	return resp
+}
+func makeUnknownAlert(remoteAddr net.Addr,localAddr net.Addr) string{
+	meta := make(map[string]string)
+	meta["remote_address"] = remoteAddr.String()
+	meta["message"] = "An unknown application protocol has connected to this device"
+	meta["local_address"] = localAddr.String()
 	return alert.NewSplunkAlertMessage(meta)
 }
 
 var mylogger *log.Logger
-func main(){
-	mylogger := logger.NewLogger("",0)
-	//Add toml stuff
+var config Config
 
-	l, e := net.Listen("tcp",":9000")
+func main(){
+	mylogger := logger.NewLogger("",0)	
+	configPath := flag.String("config","","config file")
+	flag.Parse()
+
+	if _, err := toml.DecodeFile(*configPath, &config); err != nil {
+		mylogger.Fatal("cannot decode file",err)
+	}
+
+	l, e := net.Listen("tcp",config.Address+":"+strconv.Itoa(config.Port))
 	if e != nil {
 		mylogger.Fatal("dnp listen error",e)
 	}
@@ -55,6 +92,6 @@ func main(){
 		if e != nil {
 			mylogger.Fatal("dnp failed connection",e)
 		}
-		go handler(conn)
+		go handler(conn,mylogger)
 	}	
 }
